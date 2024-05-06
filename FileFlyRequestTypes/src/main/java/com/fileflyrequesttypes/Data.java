e2c62f17fd0872f59c10c2128eb0005095c6ea7d
PANAANI <joonaoskari@hotmail.com>
2024-05-06 09:27:01 +0300

    lol
    Amended a git README file

---
+++ FileFlyRequestTypes/src/main/java/com/fileflyrequesttypes/Data.java
@@ -0,0 +1,7 @@
+package com.fileflyrequesttypes;
+
+import java.io.Serializable;
+
+public class Data implements Serializable {
+    private static final long serialVersionUID = 48319461893891L; 
+}
